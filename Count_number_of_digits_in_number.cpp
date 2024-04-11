0697409996689ba0db4a5d02c21bdf8e7a0139af
snehal <snehuvarbhe26@gmail.com>
2024-04-11 16:28:55 +0530

    count digits

--- Count_number_of_digits_in_number.cpp
+++ Count_number_of_digits_in_number.cpp
@@ -1,14 +1,24 @@
+//Given a number N, the task is to return the count of digits in this number.
 #include<iostream>
 using namespace std;
+int countdigits(int a)
+{
+    int count=0;
+    if(a==0)
+        return 1;
+    while(a!=0)
+    {
+        a=a/10;
+        count+=1;
+    }
+    return count;
+}
 int main()
 {
-    int n,c=0;
-    cout<<"enter number";
+    int n;
+    cout<<"Enter the number to count digits";
     cin>>n;
-    while(n>0)
-    {
-        c=c+1;
-        n=n/10;
-    }
-    cout<<"Total number of digits is: "<<c;
+    cout<<"Number of digits in "<<n<<" are "<<countdigits(n);
 }
+//time complexity=o(d),where d is the number of digits or O(log10(n))
+//auxiliary space:O(1) or constant
