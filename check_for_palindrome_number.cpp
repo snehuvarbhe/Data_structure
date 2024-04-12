4527d33026329b4ee2ddd706698610ae580b3353
snehal <snehuvarbhe26@gmail.com>
2024-04-12 12:44:16 +0530

    palindrome number

--- check_for_palindrome_number.cpp
+++ check_for_palindrome_number.cpp
@@ -1,6 +1,8 @@
 //Given an integer, write a function that returns true if the given number is palindrome, else false. For example, 12321 is palindrome, but 1451 is not palindrome. 
 #include <iostream>
 using namespace std;
+/*this function returns palindrome if the number is zero else it jumps to while loop where the number is reversed and then it is compared with 
+originl number,if both are same then it is palindrome else not a palindrome*/
 void palindrome(int a)
 {
     int q,w=0;
@@ -18,6 +20,7 @@ void palindrome(int a)
     else
         cout<<"Number is not a palindrome";
 }
+/*main function takes the input number and calls the function palindrome to check if the number is palindrome or not*/
 int main()
 {
     int n;
